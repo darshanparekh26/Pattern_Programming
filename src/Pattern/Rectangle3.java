2bc753146d6d914d6fdc9b7916796e1ee00e619d
darshanparekh26 <darshanparekh6306@gmail.com>
2024-10-16 16:15:32 +0530

    add n=10;

--- src/Pattern/Rectangle3.java
+++ src/Pattern/Rectangle3.java
@@ -14,10 +14,10 @@ package Pattern;
 */
 public class Rectangle3 {
     public void print() {
-
-        for (int i = 0; i < 10; i++) {
-            for (int j = 0; j < 10; j++) {
-                if (i == 0 || i == 9 || j == 0 || j == 9) {
+        int n=10;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (i == 0 || i == n-1 || j == 0 || j == n-1) {
                     System.out.print("*");
                 } else {
                     System.out.print(" ");
